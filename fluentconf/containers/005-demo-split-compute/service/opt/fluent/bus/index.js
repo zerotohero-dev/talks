19d5f5b8753430291999a152f1072fb81417c5bc
Volkan Özçelik <volkan@o2js.com>
2015-12-19 21:54:45 -0800

    publishing data...

--- fluentconf/containers/005-demo-split-compute/service/opt/fluent/bus/index.js
+++ fluentconf/containers/005-demo-split-compute/service/opt/fluent/bus/index.js
@@ -2,28 +2,36 @@
 import { Promise } from 'bluebird';
 import { createConnection as connect } from 'amqp';
 import { put, get } from 'memory-cache';
-
-let connection = connect( { host: '192.168.99.100' } );
-let resolvers = {};
-let sequenceId = 0;
+import uuid from 'node-uuid';
 
 const HOURS = 1000 * 60 * 60;
 
+let connection = connect( { host: '192.168.99.100' } );
+let resolvers = {};
+
+let generateGuid = () => uuid.v4();
+
 let resolveSubscription = ( message ) => {
-    let data = JSON.parse( message.data.toString() );
-    let response = data.response;
-    let sequenceId = data.sequenceId;
-    let resolve = resolvers[ `r${sequenceId}` ];
+    console.log( 'Will resolve subscription' );
 
-    if ( !resolve ) { return; }
+    void message;
 
-    delete resolvers[ `r${sequenceId}` ];
-    resolve( response );
+    throw 'Not Implemented Yet!';
+
+    //let data = JSON.parse( message.data.toString() );
+    //let response = data.response;
+    //let sequenceId = data.sequenceId;
+    //let resolve = resolvers[ `r${sequenceId}` ];
+    //
+    //if ( !resolve ) { return; }
+    //
+    //delete resolvers[ `r${sequenceId}` ];
+    //resolve( response );
 };
 
-let rejectDeferred = (sequenceId, param, action, reject ) =>
+let rejectDeferred = ( requestId, param, action, reject ) =>
     setTimeout( () => {
-        delete resolvers[ `r${sequenceId}` ];
+        delete resolvers[ requestId ];
 
         reject( {
             error: true,
@@ -33,31 +41,25 @@ let rejectDeferred = (sequenceId, param, action, reject ) =>
     }, 5000 );
 
 connection.on( 'ready', () => {
-    connection.queue( 'fluent-queue', ( q ) => {
+    connection.queue( 'fluent-response-queue', ( q ) => {
         q.bind( '#' );
-        q.subscribe( 'getTags-response', resolveSubscription );
-        q.subscribe( 'getUrls-response', resolveSubscription );
+        q.subscribe( resolveSubscription );
     } );
 } );
 
-let doGet = ( param, key ) =>
+let doGet = ( key, param ) =>
     get( `${key}-${param}` ) ||
     new Promise( ( resolve, reject ) => {
-        console.log( 'inside promise body' );
-        sequenceId++;
+        let requestId = generateGuid();
 
-        resolvers[ `r${sequenceId}` ] = resolve;
+        resolvers[ requestId ] = resolve;
 
-        rejectDeferred( sequenceId, param, key, reject );
+        rejectDeferred( requestId, param, key, reject );
 
-        //console.log( 'publishing…')
-        //connection.publish( `${key}-request`, { param, sequenceId } );
-
-        console.log( connection.publish );
-        console.log( connection.sendToQueue );
-
-        throw 'exception';
-
+        connection.publish(
+            'fluent-request-queue',
+            { param, key, requestId }
+        );
     } ).then(
         ( data ) => put( `${key}-${param}`, data, 3 * HOURS )
     );
@@ -65,11 +67,11 @@ let doGet = ( param, key ) =>
 /**
  *
  */
-let getTags = ( url ) => doGet( url, 'getTags' );
+let getTags = ( url ) => doGet( 'getTags', url );
 
 /**
  *
  */
-let getUrls = ( tag ) => doGet( tag, 'getUrls' );
+let getUrls = ( tag ) => doGet( 'getUrls', tag );
 
 export { getTags, getUrls };
