20df396cf1e459fe52e9cf5442fd6cf121e9e132
Volkan Özçelik <volkan@o2js.com>
2015-12-22 07:59:33 -0800

    creaed a new section.

---
+++ fluentconf/containers/007-demo-nodejs-as-a-service/service/opt/fluent/repl/index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+/*
+ * This program is distributed under the terms of the MIT license:
+ * <https://github.com/v0lkan/talks/blob/master/LICENSE.md>
+ * Send your comments and suggestions to <me@volkan.io>.
+ */
+
+import Vantage from 'vantage';
+import { startInstrumenting, stopInstrumenting } from '../monitor';
+
+let voidInformer = { inform: () => {} };
+let informer = null;
+
+/**
+ *
+ */
+let inform = ( what ) => ( informer || voidInformer ).inform( what );
+
+/**
+ *
+ */
+let listen = () => {
+    let vantage = new Vantage();
+
+    vantage
+        .command( 'ping' )
+        .description( 'Checks vantage.' )
+        .action( function( args, callback ) {
+            this.log( 'pong.' );
+
+            callback();
+        } );
+
+    vantage
+        .command( 'start-instrumenting' )
+        .description( 'Starts gathering metrics.' )
+        .action( function( args, callback ) {
+            startInstrumenting();
+
+            this.log( 'Started instrumenting.' );
+
+            callback();
+        } );
+
+    vantage
+        .command( 'stop-instrumenting' )
+        .description( 'Stops gathering metrics.' )
+        .action( function( args, callback ) {
+            stopInstrumenting();
+
+            this.log( 'Stopped instrumenting.' );
+
+            callback();
+        } );
+
+    vantage
+        .command( 'shutdown-server' )
+        .description( 'Shuts down the server.' )
+        .action( function( /*args, callback*/ ) {
+            this.log( 'Forcefully shutting down the server.' );
+
+            process.exit( 0 );
+        } );
+
+    vantage
+        .command( 'toggle-logging' )
+        .description( 'Toggles request logging.' )
+        .action( function( args, callback ) {
+            informer = informer ? null : this;
+
+            this.log( 'Toggled logging.' );
+
+            callback();
+        } );
+
+    vantage.listen( 8015 );
+};
+
+export { inform, listen };
